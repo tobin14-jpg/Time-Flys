afb121e19e8a8d429748d64267c8bc52daea0dc1
Ice Tea <shauntobin88@gmail.com>
2021-11-07 14:30:31 +0800

    built map ui, configured anchor & co-ordinates

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { Room } from "@material-ui/icons";
 
 function App () {
     const [viewport, setViewport] = useState({
@@ -16,13 +17,25 @@ function App () {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={nextViewport => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/t-14/ckvou2e7l7ehu15qljqoqb3be"
         >
         <Marker latitude={-31.8935} longitude={115.7549} offsetLeft={-20} offsetTop={-10}>
-        <div>You are here</div>
-      </Marker>
+       
+       <Room style={{ fontSize:viewport.zoom * 7, color:"slateblue"}}/>
+       </Marker>
+       <Popup
+          latitude={-31.8935}
+          longitude={115.7549}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left" >
+          <div>You are here</div>
+        </Popup>
       </ReactMapGL>
       </div>
     );
 }
+
+
 
 export default App;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,17 +1,17 @@
-const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const jwt = require("jsonwebtoken");
+
+const secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYzNTg0NDg2NiwiaWF0IjoxNjM1ODQ0ODY2fQ.Ze3JHB7jIaIvrVU7x1Xsu06mxauUPifQjajSYTj3HhY";
+const expiration = "8h";
 
 module.exports = {
-  authMiddleware({ req }) {
-    // Allows token to be sent via req.body, req.query, or headers
+  authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
+    console.log("token", token);
 
     if (!token) {
       return req;
@@ -20,15 +20,14 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch (err) {
-      console.error(err);
-      console.log('Invalid token');
+    } catch {
+      console.log("Invalid token");
     }
-
     return req;
   },
-  signToken({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
+
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
